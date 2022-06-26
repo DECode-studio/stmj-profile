9e815350e618cbee1df6786b39d9d1837f4c1b1b
DECode.studio <decode.std@gmail.com>
2022-06-26 09:49:18 +0700

    add section web

---
+++ pages/dashboard/index.php
@@ -0,0 +1,46 @@
+<?php
+
+include "template/navbar.php";
+
+include "components/header.php";
+?>
+
+<main id="main">
+
+    <?php
+
+    include "components/about_section.php";
+
+    include "components/value_section.php";
+
+    include "components/count_section.php";
+
+    include "components/features_section.php";
+
+    include "components/services_section.php";
+
+    include "components/pricing_section.php";
+
+    include "components/faq_section.php";
+
+    include "components/portofolio_section.php";
+
+    include "components/testimonials_section.php";
+
+    include "components/team_section.php";
+
+    include "components/client_section.php";
+
+    include "components/recent_post_section.php";
+
+    include "components/contact_section.php";
+
+    ?>
+
+</main>
+
+<?php
+
+include "template/footer.php";
+
+?>
