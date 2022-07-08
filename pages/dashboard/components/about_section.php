6fc8d4c9e384bad31bdb484c302bbfbdee911c5d
DECode.studio <decode.std@gmail.com>
2022-07-08 14:28:49 +0700

    add new data

--- pages/dashboard/components/about_section.php
+++ pages/dashboard/components/about_section.php
@@ -7,15 +7,15 @@
             <div class="col-lg-6 d-flex flex-column justify-content-center" data-aos="fade-up" data-aos-delay="200">
                 <div class="content">
                     <h3>Kenapa STMJ ?</h3>
-                    <h2>Soul Tracking Mobile Kunction (STMJ) adalah solusi tepat dalam pemantauan penjelajah di Alam</h2>
-                    <p>
+                    <h2 style="text-justify: newspaper;">Soul Tracking Mobile Kunction (STMJ) adalah solusi tepat dalam pemantauan penjelajah di Alam</h2>
+                    <p style="text-align: justify !important;">
                         Soul Tracking Mobile Junction (STMJ) merupakan teknologi yang dapat memantau koordinat dan kondisi pendaki di Alam. Pemancar pada STMJ menggunakan teknologi berbasis wireless / nirkabel yang dapat menyampaikan informasi penjelajah secara langsung tanpa perantara dalam jangkauan yang luas.
                     </p>
                 </div>
             </div>
 
             <div class="col-lg-6 d-flex align-items-center" data-aos="zoom-out" data-aos-delay="200">
-                <img src="assets/img/about.jpg" class="img-fluid" alt="">
+                <img src="assets/img/traveller.jpg" class="img-fluid" alt="">
             </div>
 
         </div>
